515eac57c9ba55f4a0b9cfecd81e2dc32944481f
Nicolas Moreno <nicolasmoreno@sirius.com.ar>
2023-09-18 11:25:02 -0300

    now controller calling a single service

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,14 +1,9 @@
 class WeatherController < ApplicationController
   
   def index
-    latLon = LatLngCalculator.call(
-      Rails.application.config.open_weather_key, 
-      params[:zip_code]
-    )
     render :json => WeatherFetcher.call(
       Rails.application.config.open_weather_key,
-      latLon['lat'],
-      latLon['lon']
+      params[:zip_code]
     )
   end
     
